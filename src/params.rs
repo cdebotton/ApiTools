e70ef71b2fc28f6b3d3dbd68f75427f5e7dbaf20
Christian de Botton <cdebotton@gmail.com>
2018-12-30 16:31:17 -0500

    Create convenience method for getting params from an HTTP method

--- src/params.rs
+++ src/params.rs
@@ -1,4 +1,5 @@
 use super::dialect::typescript::Typescript;
+use serde_json::Value;
 
 #[derive(Debug)]
 pub enum ParamLocation {
@@ -18,5 +19,47 @@ impl Typescript for Param {
   fn to_typescript(&self) -> String {
     let nullable = if self.nullable { "?" } else { "" };
     format!("{}: {}{};", self.name, nullable, "type")
+  }
+}
+
+#[derive(Debug)]
+pub enum Params {
+  Contents(Vec<Param>),
+  Empty,
+}
+
+type JsonMap = serde_json::Map<String, Value>;
+
+impl Params {
+  pub fn from_method(value: &JsonMap) -> Params {
+    match &value.get("parameters") {
+      Some(Value::Array(params)) => {
+        let contents: Vec<Param> = params
+          .into_iter()
+          .map(|item| {
+            let nullable = if let Value::Bool(required) = item.get("required").unwrap() {
+              *required != true
+            } else {
+              true
+            };
+
+            let location = match item.get("in").unwrap() {
+              Value::String(string) if string == "path" => ParamLocation::Uri,
+              Value::String(string) if string == "query" => ParamLocation::QueryString,
+              _ => panic!("Invalid path location"),
+            };
+
+            Param {
+              name: item.get("name").unwrap().to_string(),
+              description: item.get("description").unwrap().to_string(),
+              nullable,
+              location,
+            }
+          })
+          .collect();
+        Params::Contents(contents)
+      }
+      _ => Params::Empty,
+    }
   }
 }
--- src/schema.rs
+++ src/schema.rs
@@ -1,9 +1,9 @@
-use super::params::{Param, ParamLocation};
+use super::params::Params;
 use serde_json::Value;
 
 #[derive(Debug)]
 pub struct Get {
-  params: Option<Vec<Param>>,
+  params: Params,
   responses: (),
 }
 
@@ -21,21 +21,20 @@ pub struct Schema {
 
 impl Schema {
   pub fn new(json: Value) -> Schema {
-    let mut paths: Vec<Endpoint> = vec![];
-    if let Value::Object(entity) = &json["paths"] {
-      for (uri, value) in entity {
-        let get = match &value.get("get") {
-          Some(Value::Object(get)) => read_get(get),
-          _ => None,
-        };
+    let entity = &json.get("paths").unwrap().as_object().unwrap();
+    let paths: Vec<Endpoint> = entity.into_iter().fold(vec![], |mut acc, (uri, value)| {
+      let get = match &value.get("get") {
+        Some(Value::Object(get)) => read_get(get),
+        _ => None,
+      };
 
-        let endpoint = Endpoint {
-          uri: uri.to_owned(),
-          get,
-        };
-        paths.push(endpoint);
-      }
-    }
+      let endpoint = Endpoint {
+        uri: uri.to_owned(),
+        get,
+      };
+      acc.push(endpoint);
+      acc
+    });
 
     Schema {
       paths,
@@ -43,40 +42,11 @@ impl Schema {
     }
   }
 }
+
 type JsonMap = serde_json::Map<String, Value>;
 
-fn read_params(value: &JsonMap) -> Option<Vec<Param>> {
-  match &value.get("parameters") {
-    Some(Value::Array(params)) => params
-      .into_iter()
-      .map(|item| {
-        let nullable = if let Value::Bool(required) = item.get("required").unwrap() {
-          *required != true
-        } else {
-          true
-        };
-
-        let location = match item.get("in").unwrap() {
-          Value::String(string) if string == "path" => ParamLocation::Uri,
-          Value::String(string) if string == "query" => ParamLocation::QueryString,
-          _ => panic!("Invalid path location"),
-        };
-
-        let param = Param {
-          name: item.get("name").unwrap().to_string(),
-          description: item.get("description").unwrap().to_string(),
-          nullable,
-          location,
-        };
-        Some(param)
-      })
-      .collect(),
-    _ => None,
-  }
-}
-
 fn read_get(value: &JsonMap) -> Option<Get> {
-  let params = read_params(value);
+  let params = Params::from_method(&value);
 
   Some(Get {
     params,
